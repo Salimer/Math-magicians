b69fa9c954e4c75db4d79d8da33a82497c379139
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-30 16:11:57 +0300

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,13 @@
 import './Calculator.scss';
+import OperatorBtns from './OperatorBtns';
+import NumbersBtns from './NumbersBtns';
 
 function Calculator() {
   return (
     <article className="calculator-container">
       <div className="screen">0</div>
-      <div className="operator">
-        <div className="button">/</div>
-        <div className="button">*</div>
-        <div className="button">-</div>
-        <div className="button">+</div>
-        <div className="button">=</div>
-      </div>
-      <div className="numbers">
-        <div className="button">AC</div>
-        <div className="button">+/-</div>
-        <div className="button">%</div>
-        <div className="button">7</div>
-        <div className="button">8</div>
-        <div className="button">9</div>
-        <div className="button">4</div>
-        <div className="button">5</div>
-        <div className="button">6</div>
-        <div className="button">1</div>
-        <div className="button">2</div>
-        <div className="button">3</div>
-        <div className="button">0</div>
-        <div className="button">.</div>
-      </div>
+      <OperatorBtns />
+      <NumbersBtns />
     </article>
   );
 }
