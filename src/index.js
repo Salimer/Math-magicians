ed1a07a8aef5d243a6c6cfe3a223fccc7b6dc0b2
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-30 14:55:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Calculator from './components/Calculator';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>,
 );
 
