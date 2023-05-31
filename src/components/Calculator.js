ee2565fe65ce3819c8f892b1bd56bd3e4b00f907
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-31 14:50:19 +0300

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,24 @@
 import './Calculator.scss';
+import calculate from '../logic/calculate';
 import OperatorBtns from './OperatorBtns';
 import NumbersBtns from './NumbersBtns';
 
 function Calculator() {
+    const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+    })
+
+    const handleButtonClick = (buttonName) => {
+        const result = calculate(calculatorData, buttonName);
+        setCalculatorData(result);
+    }
+
   return (
     <article className="calculator-container">
       <div className="screen">0</div>
-      <OperatorBtns />
+      <OperatorBtns handleButtonClick={handleButtonClick} />
       <NumbersBtns />
     </article>
   );
