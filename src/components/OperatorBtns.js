4026fe8fe9c78861d9eb775ec018f1c67c308c5e
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-31 16:15:46 +0300

    Update OperatorBtns.js

--- src/components/OperatorBtns.js
+++ src/components/OperatorBtns.js
@@ -1,15 +1,18 @@
+import PropTypes from 'prop-types';
+
 function OperatorBtns({ handleButtonClick }) {
-
-
   return (
+    <>
     <div className="operator">
-      <div className="button" onClick={handleButtonClick}>/</div>
-      <div className="button">*</div>
-      <div className="button">-</div>
-      <div className="button">+</div>
-      <div className="button">=</div>
+      <div className="button" onClick={handleButtonClick}>÷</div>
+      <div className="button" onClick={handleButtonClick}>*</div>
+      <div className="button" onClick={handleButtonClick}>-</div>
+      <div className="button" onClick={handleButtonClick}>+</div>
+      <div className="button" onClick={handleButtonClick}>=</div>
     </div>
   );
 }
+
+OperatorBtns.propTypes = { handleButtonClick: PropTypes.func.isRequired };
 
 export default OperatorBtns;
