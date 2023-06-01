9e1ecc2ac9c4a7753214d4bc5cd3ace2624175ab
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-01 13:19:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Counter from './components/try/counter';
+import Fetch from './components/try/fetch';
+import UseEffect from './components/try/useEffect';
 
 function App() {
-  return (<Calculator />);
+  return (
+    <>
+      <Calculator />
+      <Counter />
+      <Fetch />
+      <UseEffect />
+    </>
+  );
 }
 
 export default App;
