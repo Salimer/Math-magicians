2df61b8bf1f5749cdfe2f246ce84167edd86b716
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-01 14:18:30 +0300

    Update Fetch.js

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
-const FetchData = async () => {
+const FetchData = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchData = async () => {
       try {
         setLoading(true);
-        const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=alone');
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=alone', {
+          headers: {
+            'X-Api-Key': 'vZdI6eGz2cS+jTb71dvouA==X8Xd3lMbKviCRmqW',
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Request failed');
+        }
         const fetchedData = await response.json();
         setData(fetchedData);
         setLoading(false);
@@ -18,8 +25,9 @@ const FetchData = async () => {
         setLoading(false);
       }
     };
-    fetchUsers();
-  }, [setData, setLoading, setError]);
+
+    fetchData();
+  }, []);
 
   if (error) return <div>Something went wrong!</div>;
 
@@ -28,7 +36,9 @@ const FetchData = async () => {
   return (
     <ul>
       {data.map((item) => (
-        <li key={item.id}>{item.title}</li>
+        <li key={1}>
+          {item.quote}
+        </li>
       ))}
     </ul>
   );
