caf6f94b4e6a3e62781c2220f503457940b975d9
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-06 18:27:18 +0300

    Create Home.js

---
+++ src/routes/Home.js
@@ -0,0 +1,16 @@
+import '../styles/routes.scss';
+
+const Home = () => (
+  <div className="home-page">
+    <h3>Welcome to our page!</h3>
+    <p>
+      lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+  </div>
+);
+export default Home;
