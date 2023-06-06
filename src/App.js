293a80b0e2cc547c048ec0ca83627fec12800af3
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-06 18:27:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
-import Calculator from './components/Calculator';
-import Fetch from './components/Fetch';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <>
-      <Calculator />
-      <Fetch />
-    </>
-  );
-}
+import Home from './routes/Home';
+import Quote from './routes/Quote';
+import Calculate from './routes/Calculate';
+import NotMatch from './routes/NotMatch';
+import Layout from './components/Layout';
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="quote" element={<Quote />} />
+      <Route path="calculate" element={<Calculate />} />
+      <Route path="*" element={<NotMatch />} />
+    </Route>
+  </Routes>
+);
 
 export default App;
