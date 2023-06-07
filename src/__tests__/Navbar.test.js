fcb17b1043b18a72717f645040d1826191aba3b5
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-07 13:18:26 +0300

    Create Navbar.test.js

---
+++ src/__tests__/Navbar.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+test('renders Navbar correctly', () => {
+  const { container } = render(
+    <MemoryRouter>
+      <Navbar />
+    </MemoryRouter>
+  );
+  expect(container).toMatchSnapshot();
+});
